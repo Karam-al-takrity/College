4bcb11de8ce60818ccb8113d94954ea506c075ba
Karam-al-takrity <karam.tk20@gmail.com>
2025-01-10 15:20:06 +0300

    addtocart , dashboard colors

--- pages/cart.js
+++ pages/cart.js
@@ -1,55 +1,58 @@
 let cart = [];
-
+let btn;
 function addToCart(product, button) {
   // Check if the product is already in the cart
-  const existingProduct = cart.find(item => item.name === product.name);
+  const existingProduct = cart.find((item) => item.name === product.name);
   if (existingProduct) {
-    alert('This product is already in your cart.');
+    alert("This product is already in your cart.");
     return;
   }
 
+  btn = button;
   // Add the product to the cart if it doesn't exist
   cart.push(product);
   updateCartCount();
   updateCartTotal();
 
   // Change button text to "Added to Cart" and disable it
-  button.innerText = 'Added to Cart';
-  button.classList.add('added'); // Add a class for styling
+  button.innerText = "Added to Cart";
+  button.classList.add("added"); // Add a class for styling
   button.disabled = true; // Disable the button to prevent further clicks
 }
 
 function updateCartCount() {
-  document.getElementById('cart-count').innerText = cart.length;
+  document.getElementById("cart-count").innerText = cart.length;
 }
 
 function updateCartTotal() {
   let total = 0;
-  cart.forEach(product => {
+  cart.forEach((product) => {
     total += product.price;
   });
-  document.getElementById('cart-total').innerText = total.toFixed(2);
+  document.getElementById("cart-total").innerText = total.toFixed(2);
   displayCartItems();
 }
 
 function displayCartItems() {
-  const cartItems = document.getElementById('cart-items');
-  cartItems.innerHTML = '';
+  const cartItems = document.getElementById("cart-items");
+  cartItems.innerHTML = "";
   cart.forEach((product, index) => {
-    const li = document.createElement('li');
-    li.innerHTML = `${product.name} - $${product.price.toFixed(2)} <button onclick="removeFromCart(${index})">Remove</button>`;
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price.toFixed(
+      2
+    )} <button onclick="removeFromCart(${index})">Remove</button>`;
     cartItems.appendChild(li);
   });
 }
 
-function removeFromCart(index) {
+function removeFromCart(index, button) {
+  btn.innerText = "🛒 Add to Cart";
   cart.splice(index, 1);
   updateCartCount();
   updateCartTotal();
 }
 
 function toggleCart() {
-  const cartModal = document.getElementById('cartModal');
-  cartModal.classList.toggle('active');
+  const cartModal = document.getElementById("cartModal");
+  cartModal.classList.toggle("active");
 }
-
